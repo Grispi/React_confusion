b6060f1d06acda88bc80750c202776f80d0b7e06
Grisel Alpanes <griselmelina@gmail.com>
2019-11-23 21:19:47 +0000

    Updates

---
+++ precache-manifest.cc15bdfec03aabc47faf03ef4f8d8efb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb91263588937fcdc44f7165c306e126",
+    "url": "/React_confusion/index.html"
+  },
+  {
+    "revision": "ee5970f6109b54f7a950",
+    "url": "/React_confusion/static/css/2.dac48fc0.chunk.css"
+  },
+  {
+    "revision": "3c6bed4bb3ce99708128",
+    "url": "/React_confusion/static/css/main.63dcf99c.chunk.css"
+  },
+  {
+    "revision": "ee5970f6109b54f7a950",
+    "url": "/React_confusion/static/js/2.65b1768d.chunk.js"
+  },
+  {
+    "revision": "3c6bed4bb3ce99708128",
+    "url": "/React_confusion/static/js/main.33e9f306.chunk.js"
+  },
+  {
+    "revision": "ea961f3f419aba7af7db",
+    "url": "/React_confusion/static/js/runtime-main.8d665e40.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React_confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React_confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React_confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React_confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React_confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
